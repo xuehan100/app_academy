c035357f8c8971daf7c79694e04fbd95c0106942
xuehan <xuehan868@gmail.com>
2020-04-23 22:03:57 -0700

    add containers in index.html

--- week3/ghost_project/Game.rb
+++ week3/ghost_project/Game.rb
@@ -1,3 +1,28 @@
 class Game 
+    def intitialize(players, fragment, dictionary)
+        @players = players
+        @fragment = fragment
+        @dictionary = dictionary
+    end
 
+    def play_round
+    end
+
+    def current_play
+
+    end
+
+    def previous_play
+
+    end
+
+    def next_player!
+    
+    end
+
+    def take_turn(player)
+
+    end
+
+    
 end
